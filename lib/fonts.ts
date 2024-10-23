faf10bdc899703a3343bd94cbf14fee2a1bea389
Iranzi Thierry <115815954+iranzithierry@users.noreply.github.com>
2024-10-23 12:34:08 +0200

    adjusting the best font

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,11 +1,13 @@
-import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google";
-
-export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
-
-export const fontMono = FontMono({
-  subsets: ["latin"],
-  variable: "--font-mono",
+import localFont from 'next/font/local';
+export const fontSans = localFont({
+  variable: '--font-sans',
+  preload: true,
+  display: "swap",
+  src: [
+    {
+      path: '../public/fonts/inter-variable.woff2',
+      weight: '100 900',
+      style: 'normal',
+    },
+  ],
 });
