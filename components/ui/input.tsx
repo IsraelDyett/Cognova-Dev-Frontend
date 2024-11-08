6b90cc773ad16f749e4c284fd34c84af591b5e27
Iranzi Thierry <115815954+iranzithierry@users.noreply.github.com>
2024-11-08 16:07:41 +0200

    fix some bugs

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -57,7 +57,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         ref={ref}
         type={type === "password" && showPassword ? "text" : type}
         className={cn(
-          `block w-full ${SuffixIcon && "!pr-10"} flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50`,
+          `block w-full ${SuffixIcon && "!pr-10"} flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50`,
           props["aria-invalid"] && "ring-1 ring-red-500 focus-visible:ring-red-500",
           className,
         )}
--- lib/site.ts
+++ lib/site.ts
@@ -2,5 +2,5 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: "Cognova AI",
-  description: "Cognova AI",
+  description: "AI-Driven Chat For Smarter Decisions",
 };
