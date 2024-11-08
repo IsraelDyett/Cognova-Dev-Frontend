825ff3abf9f6a1704baa16d9d277ccd0321a9a9f
Iranzi Thierry <115815954+iranzithierry@users.noreply.github.com>
2024-11-08 03:18:53 +0200

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    apps: [
+        {
+            name: 'cognova-app',
+            script: 'npm',
+            args: 'start',
+            env: {
+                PORT: 3020,
+                NODE_ENV: 'production'
+            },
+            instances: 'max',
+            exec_mode: 'cluster',
+            watch: false,
+            max_memory_restart: '1G',
+            autorestart: true,
+            restart_delay: 1000,
+            log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+            error_file: 'logs/error.log',
+            out_file: 'logs/output.log',
+            merge_logs: true,
+            kill_timeout: 5000,
+            metrics: {
+                port: 9615
+            }
+        }
+    ]
+};
