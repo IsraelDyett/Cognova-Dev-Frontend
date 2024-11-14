1d42e457917eaef3672b063b2816a64b742b6006
Iranzi Thierry <115815954+iranzithierry@users.noreply.github.com>
2024-11-14 15:54:33 +0200

    Update layout.tsx

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -3,10 +3,7 @@ import Footer from "./_components/footer";
 
 const LandingPageLayout = ({ children }: { children: React.ReactNode }) => {
   const origin = typeof window !== "undefined" ? window.location.origin : "https://app.cognova.io";
-  const botID =
-    process.env.NODE_ENV == "development"
-      ? "bdb97a2d-6645-4821-bd8a-5532b6a1206e"
-      : "d9c3a43b-78ae-4d48-a5e4-44baa8e8253b";
+  const botID = process.env.SYS_BOT_ID || "cm3fqhmhh000708job4qfgfpn";
   return (
     <div className="flex flex-col min-h-screen bg-background">
       <Header />
