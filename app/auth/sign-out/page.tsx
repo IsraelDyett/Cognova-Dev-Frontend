211807ce00dd53a3d90d4dfaa0be147807f40e07
Troy <iranzithierry12@gmail.com>
2024-12-09 00:42:20 +0200

    still sign out not working

--- app/auth/sign-out/page.tsx
+++ app/auth/sign-out/page.tsx
@@ -1,13 +1,28 @@
-import React from "react";
-import { siteConfig } from "@/lib/site";
+"use client";
+import { toast } from "sonner";
+import posthog from "posthog-js";
+import React, { useEffect } from "react";
+import LoadingDots from "@/components/ui/loading-dots";
 import { signOut } from "@/lib/actions/server/auth";
 
-export default async function SignOutPage() {
-	await signOut(siteConfig.domains.root);
+export default function SignOutPage() {
+	useEffect(() => {
+		if (typeof window !== "undefined") {
+			try {
+				signOut();
+			} catch (error) {
+				console.error(error, "signOut", "SignOutPage");
+				toast.error("Something goes wrong");
+			} finally {
+				posthog.capture("Signed Out");
+			}
+		}
+	}, []);
 	return (
 		<div className="flex fixed flex-col items-center justify-center h-screen bg-white inset-0">
 			<div className="flex items-end space-x-2">
 				<p className="text-base font-medium pr-2 translate-y-1.5">Logging out</p>
+				<LoadingDots />
 			</div>
 		</div>
 	);
