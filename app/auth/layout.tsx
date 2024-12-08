a0bcc091bba93c1123fa3db0e775e82af8839e88
Troy <iranzithierry12@gmail.com>
2024-12-09 00:30:12 +0200

    fix a bug and workflow error

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -4,16 +4,13 @@ import DotPattern from "@/components/ui/dot-pattern";
 
 export default function AuthLayout({
 	children,
-	classNames,
 }: {
 	children: React.ReactNode;
-	classNames?: string;
 }) {
 	return (
 		<section
 			className={cn(
 				"flex h-[100dvh] flex-col items-center justify-center w-full px-6",
-				classNames,
 			)}
 		>
 			<div className="mx-auto w-full max-w-[25rem] z-10 overflow-hidden">{children}</div>
