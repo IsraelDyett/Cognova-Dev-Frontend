2e616f6a84feeee28b38d6766174fa5aa8c6cf2c
Troy <iranzithierry12@gmail.com>
2024-12-16 10:28:56 +0200

    s

--- app/(app)/onboarding/page.tsx
+++ app/(app)/onboarding/page.tsx
@@ -1,44 +1,21 @@
+"use client";
 import React from "react";
-import { Metadata } from "next";
 import CreateWorkspaceDialog from "./components/create-workspace-dialog";
+import dynamic from "next/dynamic";
 
-export const metadata: Metadata = {
-	title: "Onboarding",
-	description: "Get started by creating the workspace",
-};
 export default function OnBoardingPage() {
+	const CreateBusinessModal = dynamic(() => import("../(workspace)/[workspaceId]/businesses/components/form")
+		.then((mod) => mod.BusinessForm), { ssr: false })
+
+	const CreateProductsModal = dynamic(() =>
+		import("../(workspace)/[workspaceId]/businesses/[businessId]/products/components/form")
+			.then((mod) => mod.ProductForm), { ssr: false })
+
+	const CreateBotModal = dynamic(() =>
+		import("../(workspace)/[workspaceId]/businesses/[businessId]/bots/components/form")
+			.then((mod) => mod.BotForm), { ssr: false })
+
 	return (
-		<div className="flex items-center min-h-[100dvh] justify-center relative">
-			<svg
-				className="absolute inset-0 -z-10 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
-				aria-hidden="true"
-			>
-				<defs>
-					<pattern
-						id="83fd4e5a-9d52-42fc-97b6-718e5d7ee527"
-						width={200}
-						height={200}
-						x="50%"
-						y={-1}
-						patternUnits="userSpaceOnUse"
-					>
-						<path d="M100 200V.5M.5 .5H200" fill="none" />
-					</pattern>
-				</defs>
-				<svg x="50%" y={-1} className="overflow-visible fill-gray-50">
-					<path
-						d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
-						strokeWidth={0}
-					/>
-				</svg>
-				<rect
-					width="100%"
-					height="100%"
-					strokeWidth={0}
-					fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)"
-				/>
-			</svg>
-			<CreateWorkspaceDialog />
-		</div>
+		<CreateWorkspaceDialog />
 	);
 }
